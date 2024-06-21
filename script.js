9b30bc9b68eabe9033fec6782d0403fe9e2d7344
IvaGjeorgjievska <igeorgievska2@gmail.com>
2024-06-21 18:10:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menu(){
+    const m = document.querySelector(".hamburger-menu-links");
+    const i = document.querySelector(".hamburger-icon");
+    m.classList.toggle("open");
+    i.classList.toggle("open");
+}
